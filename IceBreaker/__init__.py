06dd3e211943c5effc4e817014180558f2700638
Sebastian Koza <kozasebastian45@gmail.com>
2023-04-11 17:00:47 +0200

    Basic fixes

--- IceBreaker/__init__.py
+++ IceBreaker/__init__.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+print(app.template_folder)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    print(request.url)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Hello"
 
 if __name__ == '__main__':
     app.run(debug=True)

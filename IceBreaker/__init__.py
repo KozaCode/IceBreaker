d0ee681acbae9ca08efa81bf780cb9fc41d968b2
Sebastian Koza <kozasebastian45@gmail.com>
2023-04-13 13:48:02 +0200

    Implemented user session

--- IceBreaker/__init__.py
+++ IceBreaker/__init__.py
@@ -1,16 +1,38 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, session, redirect
+import uuid
 
 app = Flask(__name__)
-print(app.template_folder)
+app.secret_key = "asdf1234" #TODO:CHANGE THIS TO SOMETHING MORE SECURE
+
+# @app.route('/favicon.ico')
+# def favicon():
+#     return redirect('/static/favicon.ico')
 
 @app.errorhandler(404)
 def page_not_found(e):
     print(request.url)
+    
+#Handle form request
+@app.route('/submit-form', methods=['POST'])
+def handle_form():
+    print("#"*80)
+    print("user_id: %s" % session['user_id'])
+    print('user_id' in session)
+    if 'user_id' in session:
+        print("Submitted form with name: %s" % request.form['name'])
+        session['name'] = request.form['name']
+        return redirect('/')
 
 @app.route('/')
 def index():
-    return "Hello"
+    
+    if 'user_id' not in session:
+        session['user_id'] = uuid.uuid4()
+        session['name'] = None
+        return render_template('index.html')
+    else:
+        print("user_id: %s" % session['user_id'])
+        return render_template('index.html', message="Welcome back %s" % session['name'])
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ IceBreaker/static/js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // const socket = io();
+    // function getSessionId() {
+    //     var cookies = document.cookie.split(';');
+    //     for (var i = 0; i < cookies.length; i++) {
+    //       var cookie = cookies[i].trim();
+    //       if (cookie.startsWith('session_id=')) {
+    //         return cookie.substring('session_id='.length);
+    //       }
+    //     }
+    //     return null;
+    //   }
+    //   socket.on('connected', function() {
+    //     console.log('Connected!');
+    //     socket.emit('send_', {session_id: sessionId});
+    //   });
+    //   var sessionId = getSessionId();
+
+
+});

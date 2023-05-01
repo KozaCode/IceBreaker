47e70544b2ca2509c237b24bc112eb307711984b
Sebastian Koza <kozasebastian45@gmail.com>
2023-05-01 02:28:57 +0200

    Try catch added to floating window script

--- IceBreaker/static/js/floating_window.js
+++ IceBreaker/static/js/floating_window.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const socket = io();
-    console.log("Paired");
-    document.getElementById('user-id').innerHTML = data.user_id;
-    document.getElementById('user-name').innerHTML = data.user_name;
-    document.getElementById('partner-id').innerHTML = data.partner_id;
-    document.getElementById('partner-name').innerHTML = data.partner_name;
+    try{
+        const socket = io();
+        console.log("socket connected");
+        document.getElementById('user-id').innerHTML = data.user_id;
+        document.getElementById('user-name').innerHTML = data.user_name;
+        document.getElementById('partner-id').innerHTML = data.partner_id;
+        document.getElementById('partner-name').innerHTML = data.partner_name;
+    }catch{
+        console.log("socket not connected or data not found")
+    }
 });
 

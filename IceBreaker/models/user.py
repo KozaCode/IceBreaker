99a51db0293e25b1f784b27e8e633c0f119b8b72
Sebastian Koza <kozasebastian45@gmail.com>
2023-06-22 16:04:36 +0200

    Gender and age can be null now

--- IceBreaker/models/user.py
+++ IceBreaker/models/user.py
@@ -7,11 +7,10 @@ class User(db.Model, UserMixin):
     id = db.Column(db.String(32), primary_key=True, default=lambda: uuid.uuid4().hex, unique=True, nullable=False)
     socket_id = db.Column(db.String(20), unique=True, nullable=True)
     paired = db.Column(db.Boolean, unique=False, nullable=False, default=False)
-    
     user_name = db.Column(db.String(32), unique=False, nullable=False)
     waiting = db.Column(db.Boolean, unique=False, nullable=False, default=False)
-    gender = db.Column(db.String(32), unique=False, nullable=False)
-    age = db.Column(db.Integer, unique=False, nullable=False)
+    gender = db.Column(db.String(32), unique=False, nullable=True) #Needed only for random pairing
+    age = db.Column(db.Integer, unique=False, nullable=True) #Needed only for random pairing
     gender_pref = db.Column(db.String(32), unique=False, nullable=True)
     min_age_pref = db.Column(db.Integer, unique=False, nullable=True)
     max_age_pref = db.Column(db.Integer, unique=False, nullable=True)
